a6014a235d8c58ac9aec3ff2a74daec057a7baba
xMurdererr <62215220+xMurdererr@users.noreply.github.com>
2023-10-25 19:48:30 +0100

    Added user-defined start button

--- main/number-memory.py
+++ main/number-memory.py
@@ -11,6 +11,7 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.chrome.options import Options
+import keyboard
 
 user_start_button = 's'
 
