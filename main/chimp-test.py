aaecb4bddb89749975b8482c7aae91473917d2d6
xMurdererr <62215220+xMurdererr@users.noreply.github.com>
2023-10-25 17:45:01 +0100

    Added libraries

--- main/chimp-test.py
+++ main/chimp-test.py
@@ -3,6 +3,12 @@ Bot for https://humanbenchmark.com/tests/chimp
 To stop the bot, press Ctrl+C - the website will stay open
 """
 
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.chrome.options import Options
+
 browser_options = Options()
 browser_options.add_experimental_option('excludeSwitches', ['enable-logging'])
 browser_options.add_experimental_option("detach", True)
