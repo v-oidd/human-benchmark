86416f156bb0d856e3264095afc25e045f8720f0
xMurdererr <62215220+xMurdererr@users.noreply.github.com>
2023-10-25 19:47:24 +0100

    Update visual-memory.py

--- main/visual-memory.py
+++ main/visual-memory.py
@@ -1,6 +1,8 @@
 """
 Bot for https://humanbenchmark.com/tests/memory
 This will open a chrome tab where you are not signed in
+Log in before starting the bot to save your results
+Press 's' while on the start menu to start the bot
 Press Ctrl+C and log in to save your results
 """
 
@@ -10,6 +12,9 @@ from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.chrome.options import Options
 import time
+import keyboard 
+
+user_start_button = 's'
 
 browser_options = Options()
 browser_options.add_experimental_option('excludeSwitches', ['enable-logging'])
@@ -20,6 +25,8 @@ driver.get("https://humanbenchmark.com/tests/memory")
 
 cookie_popup = WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.XPATH, '//span[text()="AGREE"]')))
 cookie_popup.click()
+
+keyboard.wait(user_start_button)
 
 start_button = WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.XPATH, '//button[text()="Start"]')))
 start_button.click()
