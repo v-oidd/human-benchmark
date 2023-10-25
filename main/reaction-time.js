03820f305101befb57fb56a0451447b909373539
xMurdererr <62215220+xMurdererr@users.noreply.github.com>
2023-10-25 14:29:59 +0100

    Create reaction-time.js

---
+++ main/reaction-time.js
@@ -0,0 +1,36 @@
+/* Bot for https://humanbenchmark.com/tests/reactiontime
+ * Paste the script in the console once you are on the above link, before the begin the test
+ * Guaranteed 1-3ms & 100.0% percentile
+ */
+
+// Mouse down event
+function triggerMouseDown(element) {
+    const mouseDownEvent = new MouseEvent('mousedown', {
+        'view': window,
+        'bubbles': true,
+        'cancelable': true,
+    })
+    element.dispatchEvent(mouseDownEvent);
+}
+
+// Click screen if screen is green or blue
+function callback(mutationList) {
+	for (const mutation of mutationList) {
+		if (mutation.attributeName === "class") {
+			const target = mutation.target;
+            if (target.classList.contains('view-go') || target.classList.contains('view-result')) {
+				triggerMouseDown(target);
+			}
+		}
+	}
+};
+
+const observer = new MutationObserver(callback);
+const screenElement = document.querySelector('.e18o0sx0')
+const config = {
+	attributeOldValue: true,
+	attributes: true,
+	attributeFilter: ['class']
+}
+
+observer.observe(screenElement, config);
