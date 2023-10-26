5a41a863f6735687364638f97011efd65d9f6609
xMurdererr <62215220+xMurdererr@users.noreply.github.com>
2023-10-26 16:25:45 +0100

    Create chimp-test.js

---
+++ js-scripts/chimp-test.js
@@ -0,0 +1,36 @@
+/* Bot for https://humanbenchmark.com/tests/chimp
+ * Paste the script in the console once you are on the above link, before you click 'Start Test'
+ * NOTE: 41 is the maximum possible score
+ */
+
+// Auto click continue button when it appears
+const observer = new MutationObserver(() => {
+    const continueButton = document.querySelector('.css-de05nr');
+    if (continueButton) {
+        continueButton.click();
+    }
+}); 
+
+
+observer.observe(document, {
+    childList: true,
+    subtree:   true
+});
+
+let buttonCount = 4;
+
+while (buttonCount < 41)
+{
+    // Click all numbered buttons in order
+    for (let i = 1; i <= buttonCount; i++) {
+        const numberButton = document.querySelector(`div[data-cellnumber="${i}"]`);
+        if (numberButton) {
+            numberButton.click();
+        }
+    }
+    buttonCount++;
+    // Wait for continue button to disappear before next iteration
+    while (document.querySelector('.css-de05nr')) {
+        await new Promise(r => setTimeout(r, 4));
+    }
+}
